9224d82dbc272ba3096e1541338d0d63f8e646a0
jaren.xiu <jarenmarcelino76@gmail.com>
2026-01-16 19:17:59 +0800

    Final comments for lab1

--- Lab1_MarcelinoPrediguaRamoy_code.cpp
+++ Lab1_MarcelinoPrediguaRamoy_code.cpp
@@ -1,3 +1,7 @@
+// MARCELINO, Jaren
+// PREDIGUA, Dave
+// RAMOY, Lorenzo Uriel
+
 #include <iostream>
 #include <string>
 
@@ -13,9 +17,8 @@ void q3(string name) {
 }
 
 void q4(string name) {
-    //Process
-    //Check if character is a letter and handle wrap around
-    //Add +1 to the ASCII value if it is a letter
+    // Process
+    // Wraps 'z' to 'a' and 'Z' to 'A' and increments any other letter by 1
     for (int i = 0; i < name.length(); i++) {
 
         if (name[i] == 'z') {
@@ -23,21 +26,17 @@ void q4(string name) {
         } else if (name[i] == 'Z') {
             name[i] = 'A';
         } else if ((name[i] >= 'a' && name[i] <= 'z') || (name[i] >= 'A' && name[i] <= 'Z')) {
-    
-            // Only increment if it's a letter (excluding z/Z which are handled above)
             name[i] = char(int(name[i]) + 1);
         }
-        // If it is a space ' ' or symbol, it skips all and remains unchanged
     }
-    
     cout << name << endl;
 }
 
 // Function for q5
 void char_shift(char *c) {
-    // Non-letters are ignored automatically
-    // If the letter is 'z' or 'Z', it becomes 'a' or 'A'
-    // If the letter is between 'a' and 'z' or 'A' and 'Z', it becomes the next letter
+    // Helper function that takes a pointer to a single character
+    // It modifies the character in memory to the next letter, and also
+    // handling wrap around for 'z'/'Z'
     if (*c == 'z') {
         *c = 'a';
     } else if (*c == 'Z') {
@@ -47,6 +46,9 @@ void char_shift(char *c) {
     }
 }
 
+// Performs the same letter shifting as q4 but also uses helper function 
+// char_shift to show pointer usage, wherein it passes the address of each
+// character to the helper
 void q5(string name){
     for (int i = 0; i < name.length(); i++) {
         char_shift(&name[i]); 

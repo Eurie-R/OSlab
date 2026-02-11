147643a8176b27b7c9bdeb31e9d0caa25b05d905
djpredigua <djpredigua04@gmail.com>
2026-02-11 21:43:44 +0800

    Update Lab4.cpp

--- Lab 4/Lab4.cpp
+++ Lab 4/Lab4.cpp
@@ -2,6 +2,25 @@
 // PREDIGUA, Dave
 // RAMOY, Lorenzo Uriel
 
+// Number 1
 int dummy(int x){ 
     return x * 4; 
 }
+
+// Number 2
+int dummy2(int x){ 
+    return x * 4; 
+}
+
+// Number 3
+int dummy3(int x){ 
+    return x * 4; 
+}
+
+// Number 4
+int dummy4(int x){ 
+    return x * 4; 
+}
+
+// Number 5
+

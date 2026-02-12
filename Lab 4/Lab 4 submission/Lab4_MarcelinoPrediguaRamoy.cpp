ad6a38a9155c71b4f54d090de8911b347a8ee8ab
jaren.xiu <jarenmarcelino76@gmail.com>
2026-02-12 21:33:39 +0800

    Added submission folder

---
+++ Lab 4/Lab 4 submission/Lab4_MarcelinoPrediguaRamoy.cpp
@@ -0,0 +1,28 @@
+// MARCELINO, Jaren
+// PREDIGUA, Dave
+// RAMOY, Lorenzo Uriel
+
+// Number 1
+int dummy(int x){ 
+    return x * 4; 
+}
+
+// Number 2
+int dummy2(int x){ 
+    return x * 41; 
+}
+
+// Number 3
+int dummy3(int x){ 
+    return x * 63; 
+}
+
+// Number 4
+int dummy4(int x){ 
+    return x * -5; 
+}
+
+// Number 5
+int dummy5(int x){ 
+    return x * 61; 
+}

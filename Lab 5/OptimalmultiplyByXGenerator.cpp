1ed9bc1e50166b787e25f66e27f054ef05088d14
djpredigua <djpredigua04@gmail.com>
2026-02-26 11:07:34 +0800

    Update OptimalmultiplyByXGenerator.cpp

--- Lab 5/OptimalmultiplyByXGenerator.cpp
+++ Lab 5/OptimalmultiplyByXGenerator.cpp
@@ -116,6 +116,7 @@ int main(int argc, char* argv[]) {
     string mangledName = "_Z" + to_string(nameLen) + funcName + "P8IntArray";
 
     // Assembly code using cout
+    // Header
     cout << "\t.file\t\"" << funcName << ".cpp\"" << endl;
     cout << "\t.text" << endl;
     cout << "\t.globl\t" << mangledName << endl;
@@ -124,27 +125,32 @@ int main(int argc, char* argv[]) {
     cout << ".LFB0:" << endl;
     cout << "\t.cfi_startproc" << endl;
     cout << "\tendbr64" << endl;
+    // Stack frame setup
     cout << "\tpushq\t%rbp" << endl;
     cout << "\t.cfi_def_cfa_offset 16" << endl;
     cout << "\t.cfi_offset 6, -16" << endl;
     cout << "\tmovq\t%rsp, %rbp" << endl;
     cout << "\t.cfi_def_cfa_register 6" << endl;
     
-    // Note: The original assembly still had the second parameter setup (movl %esi, -28(%rbp)). 
     // Used the multiplyBy61.s as a template
-    cout << "\tmovq\t%rdi, -24(%rbp)" << endl;
-    cout << "\tmovl\t%esi, -28(%rbp)" << endl;
-    cout << "\tmovl\t$0, -4(%rbp)" << endl;
+    // Saving the parameters and setting the loop counter
+    cout << "\tmovq\t%rdi, -24(%rbp)" << endl;              // %rdi = first argument: IntArray* p
+    cout << "\tmovl\t%esi, -28(%rbp)" << endl;              // %esi = second argument: int x
+    cout << "\tmovl\t$0, -4(%rbp)" << endl;                 // stores loop index
     cout << "\tjmp\t.L2" << endl;
     
+    // Body
+    // Address calculation
     cout << ".L3:" << endl;
-    cout << "\tmovq\t-24(%rbp), %rax" << endl;
-    cout << "\tmovq\t8(%rax), %rax" << endl;
-    cout << "\tmovl\t-4(%rbp), %edx" << endl;
-    cout << "\tmovslq\t%edx, %rdx" << endl;
-    cout << "\tsalq\t$2, %rdx" << endl;
-    cout << "\taddq\t%rdx, %rax" << endl;
-    cout << "\tmovl\t(%rax), %ecx" << endl;
+    cout << "\tmovq\t-24(%rbp), %rax" << endl;              // %rax = p
+    cout << "\tmovq\t8(%rax), %rax" << endl;                // %rax = p->data
+    cout << "\tmovl\t-4(%rbp), %edx" << endl;               // %edx = i
+    cout << "\tmovslq\t%edx, %rdx" << endl;                 // %rdx = i
+    cout << "\tsalq\t$2, %rdx" << endl;                     // %rdx = i * 4
+    cout << "\taddq\t%rdx, %rax" << endl;                   // %rax = &elements[i]
+    cout << "\tmovl\t(%rax), %ecx" << endl;                 // %ecx = elements[i]
+    // Reloading the registers
+    // We do this to make sure we are using corrent values
     cout << "\tmovq\t-24(%rbp), %rax" << endl;
     cout << "\tmovq\t8(%rax), %rax" << endl;
     cout << "\tmovl\t-4(%rbp), %edx" << endl;
@@ -155,8 +161,8 @@ int main(int argc, char* argv[]) {
     // Russian Peasant
     emitShiftAddMultiply(ops);
 
-    cout << "\tmovl\t%eax, (%rdx)" << endl;
-    cout << "\taddl\t$1, -4(%rbp)" << endl;
+    cout << "\tmovl\t%eax, (%rdx)" << endl;                 // Store it into elements[i]
+    cout << "\taddl\t$1, -4(%rbp)" << endl;                 // Increment i (loop counter)
     
     cout << ".L2:" << endl;
     cout << "\tmovq\t-24(%rbp), %rax" << endl;
@@ -164,11 +170,13 @@ int main(int argc, char* argv[]) {
     cout << "\tcmpl\t%eax, -4(%rbp)" << endl;
     cout << "\tjl\t.L3" << endl;
     
+        
+    // Metadata
     cout << "\tnop" << endl;
     cout << "\tnop" << endl;
     cout << "\tpopq\t%rbp" << endl;
     cout << "\t.cfi_def_cfa 7, 8" << endl;
-    cout << "\tret" << endl;
+    cout << "\tret" << endl;                                // Return to caller
     cout << "\t.cfi_endproc" << endl;
     
     cout << ".LFE0:" << endl;
